7358dab5805b3f723108c93a5027831a387a7ada
Michaluszeq <95742362+Michaluszeq@users.noreply.github.com>
2024-01-05 11:20:56 +0100

    Panele do logowania
    Panel Do logowania  i zarejestrowania plus ikonki

--- cetus-pro-praktyki/src/App.jsx
+++ cetus-pro-praktyki/src/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import './App.css';
-import Baner from './baner/baner';
-import BottomBaner from './bottomBaner/bottomBaner';
+// import Baner from './baner/baner';
+// import BottomBaner from './bottomBaner/bottomBaner';
 
 
-export default function App() {
-  return (
-    <div className="App">
-      <Baner/>
-      <BottomBaner/>
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <Baner/>
+//       <BottomBaner/>
       
+//     </div>
+//   );
+// }
+
+
+// import RegisterPanel from './RegisterPanel';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Moja Aplikacja React</h1>
+//       {/* Dodaj panel rejestracji */}
+//       <RegisterPanel />
+//     </div>
+//   );
+// };
+
+import LoginPanel from './LoginPanel';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Moja Aplikacja React</h1>
+      {/* Dodaj panel Logowania */}
+      <LoginPanel />
     </div>
   );
-}
+};
 
-
+export default App;
