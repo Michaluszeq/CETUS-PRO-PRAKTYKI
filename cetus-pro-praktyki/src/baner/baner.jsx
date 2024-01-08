af7c19834ac42b6a9544466af3de1a54284dae00
MaciejNowicki2005 <maciejn2005@gmail.com>
2024-01-08 08:37:56 +0100

    08.01.2024 Small visual changes
    Search input css updated

--- cetus-pro-praktyki/src/baner/baner.jsx
+++ cetus-pro-praktyki/src/baner/baner.jsx
@@ -3,42 +3,51 @@ import React from 'react';
 import MessageIcon from '../Icons/message-icon.png';
 import AccountIcon from '../Icons/account-icon.png';
 import loginIcon from '../Icons/loginIcon.png';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPanel from "../loginPanel/loginPanel";
 
 export default function Baner() {
   return (
     <header className="App-header">
-    <div className="header-content">
-      <h1 className='appTitle'>StudyBuddy</h1>
-      <div className="header-icons">
-        <a href="#" className="add-listing-btn">Dodaj Ogłoszenie</a>
-        <div className="icon">
-          <img src={MessageIcon} alt="Wiadomości" />
-          <p className="icon-text">Wiadomości</p>
-        </div>
+      <div className="header-content">
+        <h1 className='appTitle'>StudyBuddy</h1>
+        <div className="header-icons">
+          <a href="#" className="add-listing-btn">Dodaj Ogłoszenie</a>
 
-    
+          
 
-        <div className="icon">
-          <img src={loginIcon} alt="Twoje Konto" />
-          <p className="icon-text">Twoje Konto</p>
-        </div>
+          <div className="icon">
+            <img src={MessageIcon} alt="Wiadomości" />
+            <p className="icon-text">Wiadomości</p>
+          </div>
 
 
 
+          <div className="icon">
+            <img src={loginIcon} alt="Twoje Konto" />
+            <p className="icon-text">Twoje Konto</p>
+          </div>
 
-        <div className="icon">
-          <img src={AccountIcon} alt="Twoje Konto" />
-          <p className="icon-text">Twoje Konto</p>
+
+
+
+          <div className="icon">
+            <img src={AccountIcon} alt="Twoje Konto" />
+            <p className="icon-text">Twoje Konto</p>
+          </div>
+
+
+          
         </div>
       </div>
-    </div>
     </header>
   );
 }
 
 
 
-   
+
 
 
 
--- cetus-pro-praktyki/src/loginPanel/loginPanel.jsx
+++ cetus-pro-praktyki/src/loginPanel/loginPanel.jsx
@@ -4,6 +4,8 @@ import AccountIcon from '../Icons/account-icon.png';
 import fb_Logo from '../Icons/fb_Logo.png';
 import google_Logo from '../Icons/google_Logo.png';
 import apple_Logo from '../Icons/apple_Logo.png';
+import Baner from '../baner/baner';
+import BottomBaner from '../bottomBaner/bottomBaner';
 
 const LoginPanel = () => {
   const [email, setEmail] = useState('');
@@ -38,53 +40,57 @@ const LoginPanel = () => {
   };
 
   return (
-    <div className="login-panel">
-      <h2>Zaloguj się</h2>
-      <form>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
+    <div>
+      <Baner />
+      <div className="login-panel">
+        <h2>Zaloguj się</h2>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
 
-        <label htmlFor="password">Hasło:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
+          <label htmlFor="password">Hasło:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
 
-        <button type="button" onClick={handleLogin}>
-          Zaloguj
-        </button>
+          <button type="button" onClick={handleLogin}>
+            Zaloguj
+          </button>
 
-        <div className="social-login">
-          <button type="button" onClick={handleFacebookLogin}>
-            <img src={fb_Logo} alt="Facebook Icon" />
-            Zaloguj przez Facebook
-          </button>
-          <button type="button" onClick={handleGoogleLogin}>
-            <img src={google_Logo} alt="Google Icon" />
-            Zaloguj przez Google
-          </button>
-          <button type="button" onClick={handleAppleLogin}>
-            <img src={apple_Logo} alt="Apple Icon" />
-            Zaloguj przez Apple
-          </button>
+          <div className="social-login">
+            <button type="button" onClick={handleFacebookLogin}>
+              <img src={fb_Logo} alt="Facebook Icon" />
+              Zaloguj przez Facebook
+            </button>
+            <button type="button" onClick={handleGoogleLogin}>
+              <img src={google_Logo} alt="Google Icon" />
+              Zaloguj przez Google
+            </button>
+            <button type="button" onClick={handleAppleLogin}>
+              <img src={apple_Logo} alt="Apple Icon" />
+              Zaloguj przez Apple
+            </button>
+          </div>
+        </form>
+
+        <div className="options">
+          <p>
+            Nie masz konta? <a href="#register">Zarejestruj się</a>
+          </p>
+          <p>
+            <a href="#forgot-password">Nie pamiętam hasła</a>
+          </p>
         </div>
-      </form>
-
-      <div className="options">
-        <p>
-          Nie masz konta? <a href="#register">Zarejestruj się</a>
-        </p>
-        <p>
-          <a href="#forgot-password">Nie pamiętam hasła</a>
-        </p>
       </div>
+      <BottomBaner />
     </div>
   );
 };
