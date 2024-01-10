a3336f12a1ee734f6b965e43419dcff830576564
Michaluszeq <95742362+Michaluszeq@users.noreply.github.com>
2024-01-10 19:01:21 +0100

    zmiana stopki itd

--- cetus-pro-praktyki/src/bottomBaner/bottomBaner.jsx
+++ cetus-pro-praktyki/src/bottomBaner/bottomBaner.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MessageIcon from '../Icons/message-icon.png';
 import AccountIcon from '../Icons/account-icon.png';
 import "./bottomBaner.css";
+
 export default function BottomBaner() {
   return (
     <footer>
@@ -10,28 +11,22 @@ export default function BottomBaner() {
       <div className='rowContainer'>
         <p>FrontEnd</p>
         <p>BackEnd</p>
-        
       </div>
 
       <div className='rowContainer'>
         <p>Maciej Nowicki</p>
         <p>Kacper Sroczyk</p>
       </div>
+      
       <div className='rowContainer'>
         <p>Michał Kociołek</p>
         <p>Kacper Pliś</p>
-        
       </div>
+
+
       <hr />
       <p>&copy; 2024 StudyBuddy</p>
-        
-      </footer>
+    </footer>
   );
 }
-
-
-
-   
-
-
 
