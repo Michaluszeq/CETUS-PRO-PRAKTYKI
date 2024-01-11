b23cf450005515fdb83f98a241efd7632e94b567
MaciejNowicki2005 <maciejn2005@gmail.com>
2024-01-11 10:24:40 +0100

    Main page update

--- cetus-pro-praktyki/src/MainPageTile/MainPageTile.jsx
+++ cetus-pro-praktyki/src/MainPageTile/MainPageTile.jsx
@@ -4,10 +4,14 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './MainPageTile.css'
 
 
-export default function App() {
+export default function App(props) {
 
   return (
     <div className="MainPageTileContainer">
+            <h2>{props.title}</h2>
+            <img className='TilePhoto' src={props.photo}></img>
+            
+
     </div>
   );
 }
--- cetus-pro-praktyki/src/mainPageContent/mainPageContent.jsx
+++ cetus-pro-praktyki/src/mainPageContent/mainPageContent.jsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "../mainPageContent/mainPageContent.css"
-import MainPageTile from '../MainPageTile/MainPageTile'
+import MainPageTile from '../MainPageTile/MainPageTile';
+import Gielda from '../Icons/ksiazka.jpg';
+import OpinieLogo from '../Icons/stasiek.png';
 
 export default function App() {
-
+  
   return (
     <div className="mpc">
-     <MainPageTile/>
-     <MainPageTile/>
+     <MainPageTile photoloc='l' photo={Gielda} title="Giełda książek" />
+     <MainPageTile photo={OpinieLogo} title="Opinie o nauczycielach"/>
     </div>
   );
 }
