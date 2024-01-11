2977bf5e814f81250979865246f93ff730474ce6
MaciejNowicki2005 <maciejn2005@gmail.com>
2024-01-11 08:26:56 +0100

    small visual changes

--- cetus-pro-praktyki/src/Main.jsx
+++ cetus-pro-praktyki/src/Main.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import SearchBar from '../src/Search/Search'; // Zmiana ścieżki na odpowiednią
+import SearchBar from '../src/Search/Search'; 
 import './App.css';
 import Baner from './baner/baner';
 import BottomBaner from './bottomBaner/bottomBaner';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import LoginPanel from '../src/loginPanel/loginPanel';
-import Settings from './TwojeKonto/TwojeKonto';
 import MainPageContent from './mainPageContent/mainPageContent'
 
 
 export default function App() {
   const handleSearch = (searchTerm) => {
-    // Tutaj możesz umieścić logikę obsługi wyszukiwania
      console.log('Wyszukiwaniee:', searchTerm);
   };
   
