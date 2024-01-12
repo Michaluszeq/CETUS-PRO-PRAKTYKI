0c1b122171c17ff4edc889bf0086607b8a64c7ea
Michaluszeq <95742362+Michaluszeq@users.noreply.github.com>
2024-01-12 10:15:07 +0100

    wyśrodkowanie login Panel i register

--- cetus-pro-praktyki/src/adverts/adverts.jsx
+++ cetus-pro-praktyki/src/adverts/adverts.jsx
@@ -1,12 +1,16 @@
 import React from 'react'; 
 import './adverts.css';
+import Search from '../Search/Search';
+import Advert from '../advert/Advert';
 
 
 
 export default function App() {
   return (
             <div className='advertsContent'>
-                
+                <Search/>
+                <Advert/>
+                 
             </div>
            
   );
