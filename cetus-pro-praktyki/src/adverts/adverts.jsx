88e7ad2aa940844b017040c56b1376774cd6d4d2
MaciejNowicki2005 <maciejn2005@gmail.com>
2024-01-15 09:13:22 +0100

    MainPageCSS  reworked

--- cetus-pro-praktyki/src/adverts/adverts.jsx
+++ cetus-pro-praktyki/src/adverts/adverts.jsx
@@ -1,12 +1,13 @@
 import React from 'react'; 
 import './adverts.css';
+import Advert from '../advert/Advert'
 
 
 
 export default function App() {
   return (
             <div className='advertsContent'>
-                
+                <Advert></Advert>
             </div>
            
   );
