a8cd2526bed51c08d3c880a658e9db82de203b67
Kapelanq <pixel131513@gmail.com>
2024-01-23 09:46:13 +0100

    backend KP
    dodanie sesji w folderze backendKP

---
+++ cetus-pro-praktyki/Backend/BackendKP/Backend/Controllers/SessionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class SessionController : Controller
+    {
+        public const string SessionName = "_SessionName";
+        public const string SessionAge = "_SessionAge";
+
+        private readonly ILogger<SessionController> _logger;
+
+        public SessionController(ILogger<SessionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetSession")]
+        public void OnGet()
+        {
+            if(string.IsNullOrEmpty(HttpContext.Session.GetString(SessionName)))
+            {
+                HttpContext.Session.SetString(SessionName, "Test");
+                HttpContext.Session.SetInt32(SessionAge, 14);
+            }
+            var name = HttpContext.Session.GetString(SessionName);
+            var age = HttpContext.Session.GetInt32(SessionAge);
+
+            _logger.LogInformation("Session name: {Name}", name);
+            _logger.LogInformation("Session age: {Age}", age);
+        }
+            
+    }
+}
---
+++ cetus-pro-praktyki/Backend/BackendKP/Backend/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "test";
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllers();
+
+app.Run();
