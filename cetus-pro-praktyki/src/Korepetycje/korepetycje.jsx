94e2c938e897845680ab5d17b48ff51237d84b3b
MaciejNowicki2005 <maciejn2005@gmail.com>
2024-01-24 10:26:27 +0100

    Added corepetitions mechanics

--- cetus-pro-praktyki/src/Korepetycje/korepetycje.jsx
+++ cetus-pro-praktyki/src/Korepetycje/korepetycje.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './korepetycje.css';
 
-function Korepetycje() {
-  const [korepetycjeData, setKorepetycjeData] = useState([]);
+function Korepetycje(props) {
+  
 
-  useEffect(() => {
-    // Tutaj możesz umieścić kod do pobierania danych z bazy danych
-    // Przykład: fetch('URL_do_api/korepetycje').then(response => response.json()).then(data => setKorepetycjeData(data));
-  }, []); // Użyj pustej tablicy jako drugi argument, aby useEffect wykonał się tylko raz przy montowaniu komponentu
+  
 
   return (
     <div className="korepetycje-container">
-      <h1>Lista Korepetycji</h1>
+     
       <div className="korepetycje-list">
-        {korepetycjeData.map(korepetycja => (
-          <div key={korepetycja.id} className="korepetycja-card">
-            <h2>{korepetycja.temat}</h2>
-            <p>{korepetycja.opis}</p>
-            <p>Cena: {korepetycja.cena} zł</p>
-            {/* Dodaj więcej informacji o korepetycji, jeśli są dostępne */}
+        
+          <div key={props.id} className="korepetycja-card">
+            <h2>{props.temat}</h2>
+            <p>{props.opis}</p>
+            <p>Cena: {props.cena} zł</p>
+            Kontakt:{props.kontakt}
+            autor:{props.autor}
+            klasa:{props.klasa}
           </div>
-        ))}
+      
       </div>
     </div>
   );
--- cetus-pro-praktyki/src/KorepetycjePage.jsx
+++ cetus-pro-praktyki/src/KorepetycjePage.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import Baner from './baner/baner';
 import BottomBaner from './bottomBaner/bottomBaner';
 
-import Korepetycjee from './Korepetycje/korepetycje';
+import Korepetycje from './Korepetycje/korepetycje';
 
-export default function Korepetycje() {  
+export default function KorepetycjePage() {
+  const [myArray, setMyArray] = useState([]);
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("http://localhost:5213/studybuddy/corepetitions", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    setMyArray(result);
+  })
+    .catch(error => console.log('error', error));
+
+  
+  
   return (
     <div className='korepetycje'>
         <Baner/>
-      <Korepetycjee />
+        <h1>Lista Korepetycji</h1>
+        {myArray.map(item => (
+        <Korepetycje
+        temat={item.subject}
+        opis={item.description}
+        cena={item.price}
+        autor={item.author}
+        klasa={item.class}
+        kontakt={item.contact} />
+      ))}
       <BottomBaner/>
       
     </div>
